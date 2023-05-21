11ddfb9f70d0d1183f16498c821c73fde696bc37
Jitesh Mahajan <jiteshmahajan22@gmail.com>
2023-05-21 20:43:44 +0530

    display all messages on screen

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ async function getAll(){
     let data = await res.json();
 
     console.log(data);
+  
+    //DOM Operation
+    for(let item of data){
+    let parent = document.querySelector("#parent");
+    let existingElement = parent.innerHTML;
+    let newElement = `<h1>${item.message}</h1>`;
+
+    parent.innerHTML = newElement + existingElement;
+    }
 }
 
 async function add(){
-   let url = `http://127.0.0.1:4400/add`;
-   let data ={message : "HardCoded"};
+    let inputTodoTag =  document.querySelector("#todoid");
+    let todoValue = inputTodoTag.value || "hardCoded";
+   
+    let url = `http://127.0.0.1:4400/add`;
+   let data ={message : todoValue};
 
    await fetch (url, {
     method : "post",
@@ -17,4 +29,11 @@ async function add(){
         "Content-Type" : "application/json",
     },
    });
+
+   //clear the box
+   inputTodoTag.value = "";
+
+   //refresh
+   getAll();
+
 }
